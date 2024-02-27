e6c6aa6246cce45e7b6625eb22ceba4f54a9ec05
Anthony Liang <aliangdw@umich.edu>
2024-02-27 07:43:09 +0000

    clean

--- README.md
+++
@@ -1,5 +0,0 @@
-## Changepoint augmentations
-
-```
-export MUJOCO_GL=egl
-```
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="changepoint_aug",
+    version="0.0.1",
+    install_requires=["gym"],
+    packages=find_packages(),
+)
